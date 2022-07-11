10f0df9535c37d92d37ffe782becbc9cb3be4671
Mikhail882 <m.pojidaev@mail.ru>
2022-07-11 11:29:57 +0300

    Initial commit

---
+++ src/main/java/pro/sky/SkyProLesson2_8/SkyProLesson28Application.java
@@ -0,0 +1,13 @@
+package pro.sky.SkyProLesson2_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProLesson28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProLesson28Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/SkyProLesson2_8/SkyProLesson28ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.SkyProLesson2_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProLesson28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
